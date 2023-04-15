57767b337426e80e42c9567ef66c88e0be004d78
EvgenieBespalov <evg.bespaloff@yandex.ru>
2023-04-16 01:07:24 +0700

    Переименовал конвертер

--- app/src/main/java/com/example/testtaskrit/data/converter/Converter.kt
+++ app/src/main/java/com/example/testtaskrit/data/converter/Converter.kt
@@ -3,7 +3,7 @@ package com.example.testtaskrit.data.converter
 import com.example.testtaskrit.data.model.dog.DogModel
 import com.example.testtaskrit.domain.entity.DogEntity
 
-class DogConverter {
+class Converter {
     fun converDog(from: DogModel): DogEntity =
         DogEntity(
             message = from.message,
--- app/src/main/java/com/example/testtaskrit/data/repository/DogRepositoryImpl.kt
+++ app/src/main/java/com/example/testtaskrit/data/repository/DogRepositoryImpl.kt
@@ -1,13 +1,13 @@
 package com.example.testtaskrit.data.repository
 
 import com.example.testtaskrit.data.api.DogApi
-import com.example.testtaskrit.data.converter.DogConverter
+import com.example.testtaskrit.data.converter.Converter
 import com.example.testtaskrit.domain.entity.DogEntity
 import com.example.testtaskrit.domain.repository.DogRepository
 
 class DogRepositoryImpl(
     private val dogApi: DogApi,
-    private val dogConverter: DogConverter,
+    private val dogConverter: Converter,
 ) : DogRepository{
     override suspend fun getDog(): DogEntity =
         dogConverter.converDog(dogApi.getRandomDog())
